756574d48fd931bc2f1ec4e6679a4d982caa875b
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-05-04 19:46:04 -0300

    feat: Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,32 @@
 import { styled } from './styles'
 import { Inter } from 'next/font/google'
+import { HomeContainer, Product } from './styles/pages/home'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat'
-})
-
+import camiseta1 from '../pages/assents/Camisetas/1.png'
+import camiseta2 from '../pages/assents/Camisetas/2.png'
+import camiseta3 from '../pages/assents/Camisetas/3.png'
 
 export default function Home() {
   return (
-    <div>
-      <Button>Hello world</Button>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt='' />
+        <footer>
+          <strong>Camise X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt='' />
+        <footer>
+          <strong>Camise X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
