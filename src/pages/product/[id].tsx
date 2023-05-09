e151530891fb678ac11ed7f660a170f7d340d9ce
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-05-09 11:37:14 -0300

    feat: Estrutura da pagina de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,11 +1,29 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../styles/pages/product";
 
 export default function product() {
-
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { query } = useRouter() // Pegando o paramentro da URL, que no caso o ID
+  const { query } = useRouter(); // Pegando o paramentro da URL, que no caso o ID
 
   return (
-    <div>product: { JSON.stringify(query) }</div>
-  )
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>RS 79,90</span>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium
+          perspiciatis quisquam iusto autem, voluptatem itaque! Sit,
+          reprehenderit necessitatibus veritatis sint qui eligendi odit beatae,
+          molestias delectus quia, autem asperiores voluptatem!
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
