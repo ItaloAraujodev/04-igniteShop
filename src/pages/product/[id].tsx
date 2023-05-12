c400981c477eb60401c404d4d385a74df8fdbc2e
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-05-12 11:43:46 -0300

    feat: Checkout no Stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -17,10 +17,17 @@ interface IProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   };
 }
 
 export default function product({ product }: IProductProps) {
+  
+  function handleBuyProduct(){
+    console.log(product.defaultPriceId)
+  }
+  
+  
   return (
     <ProductContainer>
       <ImageContainer>
@@ -36,7 +43,7 @@ export default function product({ product }: IProductProps) {
         <h1>{product.name}</h1>
         <span>{product.price}</span>
         <p>{product.description}</p>
-        <button>Comprar agora</button>
+        <button onClick={handleBuyProduct}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   );
@@ -76,6 +83,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
           currency: "BRL",
         }).format(price.unit_amount! / 100),
         description: product.description,
+        defaultPriceId: price.id
       },
     },
     revalidate: 60 * 60 * 1, // 1hours
